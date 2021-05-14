36cf55ba2743f9c472a0fa37e1c5ba310bf454d3
shuai <liershuai.cn@gmail.com>
2021-05-14 18:11:49 +0800

    Initial commit

---
+++ src/main/java/com/example/coursemanage/CourseManageApplication.java
@@ -0,0 +1,16 @@
+package com.example.coursemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shuai
+ */
+@SpringBootApplication
+public class CourseManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coursemanage/CourseManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
