c30cffcd73f26451723f580e1660e64e4c96f7c0
shuai <liershuai.cn@gmail.com>
2021-05-14 20:26:46 +0800

    master

--- src/main/java/com/example/coursemanage/CourseManageApplication.java
+++ src/main/java/com/example/coursemanage/CourseManageApplication.java
@@ -1,11 +1,13 @@
 package com.example.coursemanage;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author shuai
  */
+@MapperScan("com.example.coursemanage.mapper")
 @SpringBootApplication
 public class CourseManageApplication {
 
---
+++ src/test/java/com/example/coursemanage/mapper/UserMapperTest.java
@@ -0,0 +1,33 @@
+package com.example.coursemanage.mapper;
+
+import com.example.coursemanage.CourseManageApplication;
+import com.example.coursemanage.entity.User;
+import com.example.coursemanage.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author 李二帅
+ * @version 1.0
+ * @Date 2021/05/14 下午 8:22
+ */
+@SpringBootTest(classes = CourseManageApplication.class)
+class UserMapperTest {
+
+    @Resource
+    UserService userService;
+
+    @Test
+    public void test1(){
+        List<User> userList = userService.list();
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+    }
+}
