5eb782c1c8e582889490bbffc853013b6ea3bbc4
shuai <liershuai.cn@gmail.com>
2021-05-18 20:56:37 +0800

    master

--- src/main/java/com/coursemanage/config/MyMvcConfig.java
+++ src/main/java/com/coursemanage/config/MyMvcConfig.java
@@ -1,7 +1,9 @@
 package com.coursemanage.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,12 +15,34 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @Date 2021/05/17 下午 8:31
  */
 @Configuration
-@EnableWebMvc
 public class MyMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/main.html").setViewName("dashboard");
+    }
+
+    /**
+     * 自定义的国际化组件就生效了
+     * @return MyLocalResolver
+     */
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
+    }
+
+    /**
+     * 添加拦截器组件
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 注册拦截器，及拦截请求和要剔除哪些请求!
+        // 我们还需要过滤静态资源文件，否则样式显示不出来
+        registry.addInterceptor(new LoginHandlerInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/index.html","/user/login","/","/css/*","/img/**","/js/**");
     }
 }
