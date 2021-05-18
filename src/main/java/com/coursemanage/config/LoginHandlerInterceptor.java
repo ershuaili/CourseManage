ac468790833086bad888f253886ff02f4bb7c5ce
shuai <liershuai.cn@gmail.com>
2021-05-18 21:16:46 +0800

    master

--- src/main/java/com/coursemanage/config/LoginHandlerInterceptor.java
+++ src/main/java/com/coursemanage/config/LoginHandlerInterceptor.java
@@ -5,14 +5,18 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//自定义拦截器
+/**
+ * 自定义拦截器
+ * @author shuai
+ */
 public class LoginHandlerInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //获取 loginUser 信息进行判断
         Object user = request.getSession().getAttribute("loginUser");
-        if(user == null){//未登录，返回登录页面
-            request.setAttribute("msg","没有权限，请先登录");
+        //未登录，返回登录页面
+        if(user == null){
+            request.setAttribute("msg","您还没有登录，请先登录");
             request.getRequestDispatcher("/index.html").forward(request,response);
             return false;
         }else{
