ca56d5c4dfcddafb034282ea4cb1491dc3c30c2d
shuai <liershuai.cn@gmail.com>
2021-05-30 23:36:35 +0800

    master

--- src/main/java/com/cms/config/MyMvcConfig.java
+++ src/main/java/com/cms/config/MyMvcConfig.java
@@ -1,12 +1,39 @@
 package com.cms.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 /**
  * Created by IntelliJ IDEA.
  *
  * @author 李二帅
  * @version 1.0
  * @Date 2021/05/26 下午 10:08
+ * 扩展使用SpringMVC
  */
-public class MyMvcConfig {
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/main.html").setViewName("dashboard");
+    }
+
+    /**
+     * 添加拦截器组件
+     */
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 注册拦截器，及拦截请求和要剔除哪些请求!
+        // 我们还需要过滤静态资源文件，否则样式显示不出来
+        registry.addInterceptor(new LoginHandlerInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/index.html","/user/login","/","/css/*","/img/**","/js/**");
+    }
 }
